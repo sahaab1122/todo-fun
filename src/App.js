090760670b1a58099836d75bd93b0e30d8bc1c75
sahaab1122 <57056596+sahaab1122@users.noreply.github.com>
2021-08-07 12:49:25 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  
+
+   
+  updateInput(key, value) {
+    // update react state
+    this.setState({ [key]: value });
+  }
+
+  addItem() {
+    // create a new item with unique id
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+ 
+    };
+
+    // copy current list of items
+    const list = [...this.state.list];
+
+    // add the new item to the list
+    list.push(newItem);
+
+    // update state with new list, reset the new item input
+    this.setState({
+      list,
+      newItem: ""
+    });
+  }
+
+  deleteItem(id) {
+    // copy current list of items
+    const list = [...this.state.list];
+    // filter out the item being deleted
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({ list: updatedList });
+  }
+  
+
+  render() {
+    return (
+      <div>
+
+      
+        
+        <div className="container">
+        <div
+          style={{padding: 30,textAlign: "left", maxWidth: 500, margin: "auto"  }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Add an Item...
+          <br />
+          <input type="text"  placeholder="Type item here" value={this.state.newItem}  onChange={e => this.updateInput("newItem", e.target.value)} />
+          <button className="add-btn btn-floating" onClick={() => this.addItem()} disabled={!this.state.newItem.length}
+          >
+            <i class="material-icons"> add</i>
+          </button>
+          <br /> <br />
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button className="btn btn-floating" onClick={() => this.deleteItem(item.id)}>
+                    <i class="material-icons">x</i>
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+ 
